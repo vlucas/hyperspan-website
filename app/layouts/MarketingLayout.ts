49c7668e34b2cbad984a426e4363a678541861dd
Vance Lucas <vance@vancelucas.com>
2025-04-11 11:01:51 -0500

    Streaming example highlighted code

--- app/layouts/MarketingLayout.ts
+++ app/layouts/MarketingLayout.ts
@@ -18,6 +18,11 @@ export default function MarketingLayout({ title, content }: { title: string; con
           href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
           rel="stylesheet"
         />
+        <!-- for code highlighting in docs... -->
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/tomorrow-night-bright.min.css"
+        />
 
         <title>${title}</title>
         ${hyperspanStyleTags()}
--- app/routes/examples/streaming.ts
+++ app/routes/examples/streaming.ts
@@ -2,6 +2,7 @@ import { html } from '@/src/hyperspan/html';
 import { createRoute } from '@/src/hyperspan/server';
 import MarketingLayout from '@/app/layouts/MarketingLayout';
 import { sleep } from '@/src/lib/sleep';
+import { highlightTS } from '@/src/lib/syntax-highlighter';
 
 export default createRoute(() => {
   const content = html`
@@ -9,6 +10,9 @@ export default createRoute(() => {
       <h1>Streaming Content Example</h1>
       <p>
         Each content chunk will stream render in and render in-place whenever it is done loading.
+        This is built into <a href="/docs/html">Hyperspan HTML Templates</a> and doesn't require any
+        special syntax to enable &mdash; it is just calling an async function as a template
+        variable.
       </p>
 
       <section class="flex gap-4">
@@ -36,6 +40,37 @@ export default createRoute(() => {
           </div>
         </div>
       </section>
+
+      <h2>Example Streaming Code</h2>
+      <p>The code to enable this streaming behavior is a simple <code>html</code> template:</p>
+      ${highlightTS(`const tmpl = html\`
+  <section class="flex gap-4">
+    <div class="card">
+      <h2>Content Block 1</h2>
+      <p>Waits 1000ms to render</p>
+      \${AsyncRenderBlock(1000, 'Rendered after 1000ms!')}
+    </div>
+
+    <div class="card">
+      <h2>Content Block 2</h2>
+      <p>Waits 5000ms to render</p>
+      \${AsyncRenderBlock(5000, 'Rendered after 5000ms!')}
+    </div>
+
+    <div class="card">
+      <h2>Content Block 3</h2>
+      <p>Waits 3000ms to render</p>
+      \${AsyncRenderBlock(3000, 'Rendered after 3000ms!')}
+    </div>
+  </section>
+  \`;`)}
+
+      <p><code>AsyncRenderBlock</code> is just a standard <code>async</code> function:</p>
+      ${highlightTS(`async function AsyncRenderBlock(waitMs: number, msg: string) {
+  await sleep(waitMs);
+
+  return html\`<div>\${msg}</div>\`;
+}`)}
     </main>
   `;
 
