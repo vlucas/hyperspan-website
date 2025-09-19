afbfc4c67a305c4c5b0c0aeb68708863c270aa00
Vance Lucas <vance@vancelucas.com>
2025-09-19 09:59:52 -0500

    Redirect non-www to www for SEO

--- hyperspan.config.ts
+++ hyperspan.config.ts
@@ -7,6 +7,23 @@ export default createConfig({
   staticFileRoot: './public',
   islandPlugins: [preactPlugin()],
   beforeRoutesAdded(app) {
+    // Redirect non-www to www
+    app.use('*', (c, next) => {
+      const host = c.req.header('host');
+      const isWww = host && host.startsWith('www.');
+      const isApp = host && host.startsWith('app.');
+      const isLocal = host && host.includes('localhost');
+
+      if (!isLocal && !isWww && !isApp) {
+        const url = new URL(c.req.url);
+        const newUrl = `https://www.${host}${url.pathname}${url.search}`;
+        return c.redirect(newUrl, 301);
+      }
+
+      return next();
+    });
+
+    // Cache docs pages
     app.use('/docs/*', memoryCacheTime('1w'));
   },
 });
