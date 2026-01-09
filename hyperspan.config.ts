24b5c66600e553daaa327d452c27e955b5122880
Vance Lucas <vance@vancelucas.com>
2026-01-09 13:22:04 -0600

    Enable non-www redirect in Hyperspan

--- hyperspan.config.ts
+++ hyperspan.config.ts
@@ -5,11 +5,11 @@ export default createConfig({
   appDir: './app',
   publicDir: './public',
   plugins: [preactPlugin()],
-  /*
-  beforeRoutesAdded(app) {
+
+  beforeRoutesAdded(server) {
     // Redirect non-www to www
-    app.use('*', (c, next) => {
-      const host = c.req.header('host');
+    server._middleware.push((c, next) => {
+      const host = c.req.headers.get('host');
       const isWww = host && host.startsWith('www.');
       const isApp = host && host.startsWith('app.');
       const isLocal = host && host.includes('localhost');
@@ -17,11 +17,11 @@ export default createConfig({
       if (!isLocal && !isWww && !isApp) {
         const url = new URL(c.req.url);
         const newUrl = `https://www.${host}${url.pathname}${url.search}`;
-        return c.redirect(newUrl, 301);
+        return c.res.redirect(newUrl, { status: 301 });
       }
 
       return next();
     });
   },
-  */
+
 });
