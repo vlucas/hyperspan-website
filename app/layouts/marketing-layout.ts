7f15ca5ad1a62a6b7dd27efa5592ce60d20c23cb
Vance Lucas <vance@vancelucas.com>
2026-01-28 19:58:32 -0600

    Layout fix

--- app/layouts/marketing-layout.ts
+++ app/layouts/marketing-layout.ts
@@ -4,14 +4,14 @@ import '@/app/styles/globals.css';
 
 import type { Hyperspan as HS } from '@hyperspan/framework';
 
-export default function MarketingLayout(c: HS.Context, { title, content, meta }: { title: string; content: any; meta: { description: string } }) {
+export default function MarketingLayout(c: HS.Context, { title, content, meta }: { title: string; content: any; meta?: { description?: string } }) {
   return html`
     <!doctype html>
     <html class="w-full h-full" lang="en" data-theme="sunset">
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="${meta.description}" />
+        <meta name="description" content="${meta?.description}" />
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎯</text></svg>"
